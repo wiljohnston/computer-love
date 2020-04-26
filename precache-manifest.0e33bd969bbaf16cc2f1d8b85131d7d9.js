8ecd8f68d9f20174d13bd805de671dc8ba2bd680
William Johnston <wjoh0002@student.monash.edu>
2020-04-26 13:50:22 +1000

    copied build contents from build folder to root folder

---
+++ precache-manifest.0e33bd969bbaf16cc2f1d8b85131d7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c994ae6affac7a1595a86089ae2089",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82873d8d74efad455d58",
+    "url": "/static/css/main.9dbc9b3a.chunk.css"
+  },
+  {
+    "revision": "fea6ed7cefe5aeb4fbff",
+    "url": "/static/js/2.a4456c80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4456c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82873d8d74efad455d58",
+    "url": "/static/js/main.eac6cdde.chunk.js"
+  },
+  {
+    "revision": "1d34b616e7949721b8f8",
+    "url": "/static/js/runtime-main.4a6ea857.js"
+  }
+]);
